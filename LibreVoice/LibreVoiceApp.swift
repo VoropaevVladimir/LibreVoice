555d14210c010a3844c396c897b9f5fa07e78f94
VoropaevVladimir <voropev@gmail.com>
2026-07-17 14:26:02 +0700

    Initial Commit

---
+++ LibreVoice/LibreVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreVoiceApp.swift
+//  LibreVoice
+//
+//  Created by Владимир on 17.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct LibreVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
